807eacf0a35e1115dccbcca3db14eaae30a4932c
dom111 <dom111@users.noreply.github.com>
2021-02-17 20:05:02 +0000

    Add missing events to trigger `Rule`s.

--- Events/Engine/start.js
+++ Events/Engine/start.js
@@ -1,18 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getEvents = void 0;
+const End_1 = require("../../Rules/End");
 const Engine_1 = require("@civ-clone/core-engine/Engine");
 const RuleRegistry_1 = require("@civ-clone/core-rule/RuleRegistry");
 const Start_1 = require("@civ-clone/core-engine/Rules/Start");
+const Start_2 = require("../../Rules/Start");
 const Turn_1 = require("../../Turn");
 const getEvents = (ruleRegistry = RuleRegistry_1.instance, engine = Engine_1.instance) => [
+    [
+        'engine:start',
+        () => {
+            ruleRegistry.process(Start_1.Start);
+        },
+    ],
     ...['game:start', 'turn:end'].map((event) => [
         event,
         () => engine.emit('turn:start', Turn_1.instance.increment()),
     ]),
-    ['engine:start', () => {
-            ruleRegistry.process(Start_1.Start);
-        }]
+    [
+        'turn:end',
+        () => {
+            ruleRegistry.process(End_1.End, Turn_1.instance);
+        },
+    ],
+    [
+        'turn:start',
+        () => {
+            ruleRegistry.process(Start_2.Start, Turn_1.instance);
+        },
+    ],
 ];
 exports.getEvents = getEvents;
 exports.default = exports.getEvents;
--- Events/Engine/start.ts
+++ Events/Engine/start.ts
@@ -1,3 +1,4 @@
+import { End, IEndRegistry } from '../../Rules/End';
 import {
   Engine,
   instance as engineInstance,
@@ -6,21 +7,37 @@ import {
   RuleRegistry,
   instance as ruleRegistryInstance,
 } from '@civ-clone/core-rule/RuleRegistry';
-import { Start, IStartRegistry } from '@civ-clone/core-engine/Rules/Start';
+import {
+  Start as EngineStart,
+  IStartRegistry as IEngineStartRegistry,
+} from '@civ-clone/core-engine/Rules/Start';
+import { Start, IStartRegistry } from '../../Rules/Start';
 import { instance as turn } from '../../Turn';
 
 export const getEvents = (
   ruleRegistry: RuleRegistry = ruleRegistryInstance,
   engine: Engine = engineInstance
 ): [string, () => any][] => [
+  [
+    'engine:start',
+    (): void => {
+      (ruleRegistry as IEngineStartRegistry).process(EngineStart);
+    },
+  ],
   ...['game:start', 'turn:end'].map((event: string): [string, () => any] => [
     event,
     (): boolean => engine.emit('turn:start', turn.increment()),
   ]),
   [
-    'engine:start',
+    'turn:end',
     (): void => {
-      (ruleRegistry as IStartRegistry).process(Start);
+      (ruleRegistry as IEndRegistry).process(End, turn);
+    },
+  ],
+  [
+    'turn:start',
+    (): void => {
+      (ruleRegistry as IStartRegistry).process(Start, turn);
     },
   ],
 ];
